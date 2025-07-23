24fa7d849989df1912d47f4a68fefa96f8bf2a13
unknown <muhammadaizaz655@gmail.com>
2025-07-23 15:41:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// function myfunction(){
+//     console.log("Welcome to Pakistan")
+// }
+// myfunction();
+
+// function sum(x,y){
+// console.log(x+y)
+// }
+
+// function sum (x,y){
+//   add = x + y;
+//   return add;
+// }
+
+// let val = sum (22,33);
+// console.log(val) 
+
+// arrow function
+// let sum = (a,b) => {
+    
+//     return a+b;
+// }
+
